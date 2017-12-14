8c6e870e538aafa5615058a3db6d96498e78f677
dimitsia <dimitsia5@teicm.gr>
2017-12-14 18:39:21 +0200

    Art_gallery_site, 1st commit

---
+++ expo/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+class Painting(models.Model):
+    artist = models.CharField(max_length=50)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+
+    def __str__(self):
+        return self.title
