ab67c2ee99656698d8de50685e23322e39d42180
dimitsia <dimitsia5@teicm.gr>
2017-12-15 00:40:32 +0200

    Templates display paintings from database

--- expo/views.py
+++ expo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Painting
 
 
 def painting_list(request):
-    return render(request, 'expo/painting_list.html', {})
+    paintings = Painting.objects.all().order_by('artist')
+    return render(request, 'expo/painting_list.html', {'paintings': paintings})
