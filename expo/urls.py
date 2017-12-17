fe8442314322ff8a516f0434ff22659e292ddad9
dimitsia <dimitsia5@teicm.gr>
2017-12-17 05:38:29 +0200

    Extend CSS & upload images

--- expo/urls.py
+++ expo/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.painting_list, name='painting_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.painting_detail, name='painting_detail'),
 ]
--- expo/views.py
+++ expo/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Painting
 
 
 def painting_list(request):
-    paintings = Painting.objects.all().order_by('artist')
-    return render(request, 'expo/painting_list.html', {'paintings': paintings})
+    paint_objects = Painting.objects.all().order_by('artist')
+    return render(request, 'expo/painting_list.html', {'paint_objects': paint_objects})
+
+
+def painting_detail(request, pk):
+    paint_object = get_object_or_404(Painting, pk=pk)
+    return render(request, 'expo/painting_detail.html', {'paint_object': paint_object})
